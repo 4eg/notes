ada3fee4091af42ce8f3ea5bb8e0bc3f9cd2d42a
Alexandre Gressier <aeg@gressier.xyz>
2018-02-22 09:43:44 +0100

    Change from int to float temperatures for "The C Programming Language" exercise 1-3

--- c/k-and-r/exercises/1-3/f_to_c.c
+++ c/k-and-r/exercises/1-3/f_to_c.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
+/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300; floating-point version */
 main()
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 
 	lower = 0;		/* lower limit of temperature table */
@@ -14,8 +14,8 @@ main()
 	
 	fahr = lower;
 	while (fahr <= upper) {
-		celsius = 5 * (fahr-32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f\t%3.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
