5133ec745a770880842ae23bd0f686c8ceb06803
Alexandre Gressier <aeg@gressier.xyz>
2018-03-10 22:19:29 +0100

    Rewrite answer to "The C Programming Language" exercise 1-22

--- c/k-and-r/exercises/1-22/fold.c
+++ c/k-and-r/exercises/1-22/fold.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
-#define MAXCOL 80
+#define MAXLINE 20
 #define TABSIZE 8
 
-void printl(char line[], int col);
-
-/*  */
+/* fold long input lines into two or more shorter lines */
 int main()
 {
-	int c, col, wlen;
-	char line[MAXCOL];
+	int c, llen, wlen, slen, toff;
+	char word[MAXLINE];
+	char spc[MAXLINE];
 
-	wlen = 0;
-	for (col = 1; (c = getchar()) != EOF; ++col) {
-		if (c == ' ') {
-			if (col + wlen > MAXCOL)
-				wlen = 1;
-		} else if (c == '\n') {
-			putchar(c);
-			col = 0;
+	toff = slen = wlen = llen = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == '\n') { /* end of a word */
+			if (wlen > 0) {
+				spc[slen] = word[wlen] = '\0';
+				if (llen + slen + toff + wlen<= MAXLINE) {
+					printf("%s", spc);
+					llen += slen + toff;
+				} else {
+					putchar('\n');
+					llen = 0; /* you could quick up and do not update llen if c == '\n' */
+				}
+				printf("%s", word);
+				llen += wlen;
+				toff = slen = wlen = 0;
+			}
+			if (c != '\n') {
+				spc[slen++] = c;
+				if (c == '\t')
+					toff += TABSIZE - 1 - llen % TABSIZE;
+			}
+			if (c == '\n' || slen + toff == MAXLINE - 1) {
+				putchar('\n');
+				slen = llen = 0; /* slen = 0: discard spaces on a line containing only spaces */
+			}				
 		} else {
-			putchar(c);
+			word[wlen++] = c;
+			if (slen + toff + wlen == MAXLINE - 1) { /* this is only entered when the line is full of non-spacing characters; -1: left for the hyphen */
+				spc[slen] = word[wlen] = '\0';
+				printf("%s%s-\n", spc, word);
+				toff = slen = wlen = llen = 0;
+			}
 		}
 	}
 	return 0;
 }
-
-/* printl: print line until col is reached */
-void printl(char s[], int col)
-{
-	int i;
-
-	for (i = 0; s < col; ++i)
-		putchar(s[i]);
-	if (i > 0)
-		putchar('\n');
-}
-
-		
